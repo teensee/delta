f982cf2257c3bed0ab34f8aa90a199866b81c0bf
teensee <Stimulmonk@yandex.ru>
2019-02-26 09:57:48 +0300

    Base AttachedProperty
    Added base class to work with attached property. Added attached property to password box that makes hide/visible a placeholder text and i save an example for a 'simple' attached property for password box which has name PasswordBoxAttachedPropertyOld

---
+++ delta/AttachedProperties/BaseAttachedProperty.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Windows;
+
+namespace delta
+{
+    /// <summary>
+    /// Base attached property class
+    /// </summary>
+    /// <typeparam name="Parent">Parent class to the attached prop.</typeparam>
+    /// <typeparam name="Property">Type of this attached property</typeparam>
+    public abstract class BaseAttachedProperty<Parent, Property>
+        where Parent: BaseAttachedProperty<Parent, Property>, new()
+    {
+        #region Public events
+
+        /// <summary>
+        /// Fired when the value changes
+        /// </summary>
+        public event Action<DependencyObject, DependencyPropertyChangedEventArgs> ValueChanged = (sender, e) => { };
+
+        #endregion
+
+        #region Singleton
+
+        /// <summary>
+        /// Singleton instance of our parent class
+        /// </summary>
+        public static Parent Instance { get; private set; } = new Parent();
+
+        #endregion
+
+        #region Attached Property Definitions
+
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.RegisterAttached(
+                "Value", 
+                typeof(Property), 
+                typeof(BaseAttachedProperty<Parent, Property>), 
+                new PropertyMetadata(new PropertyChangedCallback(OnValuePropertyChanged)));
+
+        /// <summary>
+        /// Callback event whent the <see cref="ValueProperty"/> is changed
+        /// </summary>
+        /// <param name="d">The UI element that had it's property changed</param>
+        /// <param name="e">The arguments for the event</param>
+        private static void OnValuePropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            //Call the parent function
+            Instance.OnValueChanged(d, e);
+
+            //Call event listeners
+            Instance.ValueChanged(d, e);
+        }
+
+        /// <summary>
+        /// Gets the attached property
+        /// </summary>
+        /// <param name="d">The element to get the property from</param>
+        /// <returns></returns>
+        public static Property GetValue(DependencyObject d) => (Property)d.GetValue(ValueProperty);
+
+        /// <summary>
+        /// Sets the attached property
+        /// </summary>
+        /// <param name="d">The element to get the property from</param>
+        /// <param name="value">The value to set the property</param>
+        public static void SetValue(DependencyObject d, Property value) => d.SetValue(ValueProperty, value);
+
+        #endregion
+
+        #region Event Methods
+
+        /// <summary>
+        /// The method that is called when any attached property of this type os changed
+        /// </summary>
+        /// <param name="sender">The UI element that this propperty was changed</param>
+        /// <param name="e">The argument for this event</param>
+        public virtual void OnValueChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e) { }
+
+
+
+        #endregion
+    }
+}
---
+++ delta/AttachedProperties/PasswordBoxAttachedProperties.cs
@@ -0,0 +1,62 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace delta
+{
+    /// <summary>
+    /// The MonitorPassword a <see cref="PasswordBox"/>
+    /// </summary>
+    public class MonitorPasswordProperty : BaseAttachedProperty<MonitorPasswordProperty, bool>
+    {
+        public override void OnValueChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            //Get the caller
+            var passwordBox = (sender as PasswordBox);
+
+            //Make sure it is a password box
+            if (passwordBox == null)
+                return;
+
+            //Remove any previous events
+            passwordBox.PasswordChanged -= PasswordBox_PasswordChanged;
+
+            //if the caller set MonitorPassword to true...
+            if ((bool)e.NewValue)
+            {
+                //Set default value
+                HasTextProperty.SetValue(passwordBox);
+
+                //Start listening out for password changes
+                passwordBox.PasswordChanged += PasswordBox_PasswordChanged;
+            }
+        }
+
+        /// <summary>
+        /// Fired when the password box password value changes
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            //Set the attached HasText value
+            HasTextProperty.SetValue((PasswordBox)sender);
+        }
+    }
+
+    /// <summary>
+    /// The HasText for a <see cref="PasswordBox"/>
+    /// </summary>
+    public class HasTextProperty : BaseAttachedProperty<HasTextProperty, bool>
+    {
+        /// <summary>
+        /// Sets the HasText property based on if the caller <see cref="PasswordBox"/> has any text'
+        /// We make short SetValue with 1 parametr
+        /// </summary>
+        /// <param name="sender"></param>
+        public static void SetValue(DependencyObject sender)
+        {
+            //We call SetValue from BaseAttachedProperty
+            SetValue(sender, ((PasswordBox)sender).SecurePassword.Length > 0);
+        }
+    }
+}
